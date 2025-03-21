96b6ed6fdf3e796a6e3009487fe28e54aecd65df
Shuhaib77 <shuhaibbasheer20022@gmail.com>
2025-03-21 14:58:32 +0530

    updated

--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 function Landing() {
-  const navigate=useNavigate()
+  const navigate = useNavigate();
   return (
     <>
       <div className="flex justify-center items-center h-[86vh]">
         <div className="text-center">
           <h1 className="text-8xl font-medium ">
-            Welcome to <span className="underline decoration-amber-600 decoration-5">TSEEP Mastery Box</span> 
+            Welcome to{" "}
+            <span className="underline decoration-amber-600 decoration-5">
+              TSEEP Mastery Box
+            </span>
           </h1>
-          <h1 className="text-gray-400 text-3xl">
+          <h1 className="text-gray-400 text-center text-3xl">
             Unlock Your Potentiol with AI inspired tool
           </h1>
 
@@ -24,9 +27,12 @@ function Landing() {
               </label>
             </div>
             <div className="w-1/2 text-end">
-              <button className="w-[130px] rounded bg-blue-950 pt-3 pb-3 text-white " onClick={()=>{
-                navigate('/register')
-              }}>
+              <button
+                className="w-[130px] rounded bg-blue-950 pt-3 pb-3 text-white "
+                onClick={() => {
+                  navigate("/register");
+                }}
+              >
                 Get Start
               </button>
             </div>
